46678c1d3e012bfdce95f714d663d325eb20f8d1
Bill Thornton <bill@thornbill.dev>
2021-09-15 00:04:48 -0400

    Add typedoc for documentation

--- src/models/client-info.ts
+++ src/models/client-info.ts
@@ -3,6 +3,10 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/.
  */
+
+/**
+ * An interface representing a client application.
+ */
 export interface ClientInfo {
 	Name: string,
 	Version: string
--- src/models/device-info.ts
+++ src/models/device-info.ts
@@ -3,6 +3,10 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/.
  */
+
+/**
+ * An interface representing a client device.
+ */
 export interface DeviceInfo {
 	Id: string,
 	Name: string
