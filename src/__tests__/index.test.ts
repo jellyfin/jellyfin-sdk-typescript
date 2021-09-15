e43088157385c986798dd87286ef3775ca40ef9d
Bill Thornton <bill@thornbill.dev>
2021-09-15 01:30:55 -0400

    Update structure

--- src/__tests__/index.test.ts
+++ src/__tests__/index.test.ts
@@ -10,33 +10,37 @@ const TEST_CONFIGURATION = new Configuration({
 });
 
 test('Test the BaseAPI constructor', () => {
-	const jellyfin = new Jellyfin(TEST_CONFIGURATION);
+	const jellyfin = new Jellyfin();
 	expect(jellyfin).not.toBeNull();
 
-	expect(jellyfin.configuration.basePath).toEqual(SERVER_URL);
-	expect(jellyfin.authorizationHeader).toEqual('MediaBrowser Client="jellyfin-sdk-typescript", Device="device-name", DeviceId="device-id", Version="v0.1.0", Token=""')
+	const api = jellyfin.createApi(TEST_CONFIGURATION);
+	expect(api.configuration.basePath).toEqual(SERVER_URL);
+	expect(api.authorizationHeader).toEqual('MediaBrowser Client="jellyfin-sdk-typescript", Device="device-name", DeviceId="device-id", Version="v0.1.0", Token=""')
 });
 
 test('Test public users api', async () => {
-	const jellyfin = new Jellyfin(TEST_CONFIGURATION);
+	const jellyfin = new Jellyfin();
+	const api = jellyfin.createApi(TEST_CONFIGURATION);
 
-	const users = await jellyfin.userApi.getPublicUsers();
+	const users = await api.userApi.getPublicUsers();
 	// console.log('Users =>', users.data);
 	expect(users.data).toBeTruthy();
 });
 
 test('Test public system info api', async () => {
-	const jellyfin = new Jellyfin(TEST_CONFIGURATION);
+	const jellyfin = new Jellyfin();
+	const api = jellyfin.createApi(TEST_CONFIGURATION);
 
-	const info = await jellyfin.systemApi.getPublicSystemInfo();
+	const info = await api.systemApi.getPublicSystemInfo();
 	// console.log('Info =>', info.data);
 	expect(info.data).toBeTruthy();
 });
 
 test('Test user login api', async () => {
-	const jellyfin = new Jellyfin(TEST_CONFIGURATION);
+	const jellyfin = new Jellyfin();
+	const api = jellyfin.createApi(TEST_CONFIGURATION);
 
-	const auth = await jellyfin.authenticateUserByName({ Username: 'demo', Pw: '' });
+	const auth = await api.authenticateUserByName({ Username: 'demo', Pw: '' });
 	// console.log('Auth =>', auth.data);
 	expect(auth.data).toBeTruthy();
 });
---
+++ src/utils.ts
@@ -0,0 +1,21 @@
+/**
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+import { DeviceInfo } from "./generated-client";
+import { ClientInfo } from "./models";
+
+/**
+ * Returns a valid authorization header string.
+ */
+export function getAuthorizationHeader(clientInfo: ClientInfo, deviceInfo: DeviceInfo, accessToken = ''): string {
+	// TODO: We should ensure values are properly escaped
+	return [
+		`MediaBrowser Client="${clientInfo.Name}"`,
+		`Device="${deviceInfo.Name}"`,
+		`DeviceId="${deviceInfo.Id}"`,
+		`Version="${clientInfo.Version}"`,
+		`Token="${accessToken}"`
+	].join(', ');
+}
