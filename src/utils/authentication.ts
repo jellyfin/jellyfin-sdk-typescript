527ec22fb8a347d0c16bc65bb58663eb13347df6
Bill Thornton <bill@thornbill.dev>
2021-09-16 15:38:33 -0400

    Restructure utils

--- src/utils/authentication.ts
+++ src/utils/authentication.ts
@@ -3,8 +3,7 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/.
  */
-import { DeviceInfo } from './generated-client';
-import { ClientInfo } from './models';
+import { ClientInfo, DeviceInfo } from '../models';
 
 /**
  * Returns a valid authorization header string.
