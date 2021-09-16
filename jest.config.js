5067291dbaf57a0778a84302b03d786f519bb5ba
Bill Thornton <bill@thornbill.dev>
2021-09-16 16:27:30 -0400

    Add test coverage

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,7 @@
 /** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
 module.exports = {
 	preset: 'ts-jest',
-	testEnvironment: 'node'
+	testEnvironment: 'node',
+	collectCoverageFrom: [ 'src/**' ],
+	coveragePathIgnorePatterns: [ 'generated-client' ]
 };
