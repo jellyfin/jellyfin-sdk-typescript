1f3e1c73a3c9c111120652fd75b88627d54493ac
Bill Thornton <bill@thornbill.dev>
2021-09-19 00:21:06 -0400

    Add address candidate tests

--- src/utils/__tests__/address-candidates.test.ts
+++ src/utils/__tests__/address-candidates.test.ts
@@ -9,21 +9,39 @@ import { getAddressCandidates } from '..';
 describe('Address Candidates', () => {
 	describe('getAddressCandidates()', () => {
 		it('should return candidates for a given domain', () => {
-			const candidates = getAddressCandidates('demo.jellyfin.org');
+			const candidates = getAddressCandidates('example.com');
 			expect(candidates).toHaveLength(5);
-			expect(candidates[0].toString()).toEqual('http://demo.jellyfin.org/');
-			expect(candidates[1].toString()).toEqual('https://demo.jellyfin.org/');
-			expect(candidates[2].toString()).toEqual('http://demo.jellyfin.org:8096/');
-			expect(candidates[3].toString()).toEqual('https://demo.jellyfin.org:8096/');
-			expect(candidates[4].toString()).toEqual('https://demo.jellyfin.org:8920/');
+			expect(candidates[0].toString()).toEqual('http://example.com/');
+			expect(candidates[1].toString()).toEqual('https://example.com/');
+			expect(candidates[2].toString()).toEqual('http://example.com:8096/');
+			expect(candidates[3].toString()).toEqual('https://example.com:8096/');
+			expect(candidates[4].toString()).toEqual('https://example.com:8920/');
 		});
 
 		it('should return candidates for an https url', () => {
-			const candidates = getAddressCandidates('https://demo.jellyfin.org');
+			const candidates = getAddressCandidates('https://example.com');
 			expect(candidates).toHaveLength(3);
-			expect(candidates[0].toString()).toEqual('https://demo.jellyfin.org/');
-			expect(candidates[1].toString()).toEqual('https://demo.jellyfin.org:8096/');
-			expect(candidates[2].toString()).toEqual('https://demo.jellyfin.org:8920/');
+			expect(candidates[0].toString()).toEqual('https://example.com/');
+			expect(candidates[1].toString()).toEqual('https://example.com:8096/');
+			expect(candidates[2].toString()).toEqual('https://example.com:8920/');
+		});
+
+		it('should use the specified port when provided', () => {
+			const candidates = getAddressCandidates('http://example.com:8888');
+			expect(candidates).toHaveLength(2);
+			expect(candidates[0].toString()).toEqual('http://example.com:8888/');
+			expect(candidates[1].toString()).toEqual('https://example.com:8888/');
+		});
+
+		it('should return the entered url non http(s) protocols', () => {
+			const candidates = getAddressCandidates('ftp://example.com');
+			expect(candidates).toHaveLength(1);
+			expect(candidates[0].toString()).toEqual('ftp://example.com/');
+		});
+
+		it('should return an empty list for invalid urls', () => {
+			const candidates = getAddressCandidates('/');
+			expect(candidates).toHaveLength(0);
 		});
 	});
 });
