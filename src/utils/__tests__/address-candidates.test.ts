ec8f66cf7e72759df8b6b814aa01adfa82715066
Bill Thornton <bill@thornbill.dev>
2021-10-12 09:27:49 -0400

    Fix jest linting

--- src/utils/__tests__/address-candidates.test.ts
+++ src/utils/__tests__/address-candidates.test.ts
@@ -16,32 +16,32 @@ describe('Address Candidates', () => {
 		it('should return candidates for a given domain', () => {
 			const candidates = getAddressCandidates('example.com');
 			expect(candidates).toHaveLength(5);
-			expect(candidates[0]).toEqual('https://example.com/');
-			expect(candidates[1]).toEqual('https://example.com:8096/');
-			expect(candidates[2]).toEqual('https://example.com:8920/');
-			expect(candidates[3]).toEqual('http://example.com/');
-			expect(candidates[4]).toEqual('http://example.com:8096/');
+			expect(candidates[0]).toBe('https://example.com/');
+			expect(candidates[1]).toBe('https://example.com:8096/');
+			expect(candidates[2]).toBe('https://example.com:8920/');
+			expect(candidates[3]).toBe('http://example.com/');
+			expect(candidates[4]).toBe('http://example.com:8096/');
 		});
 
 		it('should return candidates for an https url', () => {
 			const candidates = getAddressCandidates('https://example.com');
 			expect(candidates).toHaveLength(3);
-			expect(candidates[0]).toEqual('https://example.com/');
-			expect(candidates[1]).toEqual('https://example.com:8096/');
-			expect(candidates[2]).toEqual('https://example.com:8920/');
+			expect(candidates[0]).toBe('https://example.com/');
+			expect(candidates[1]).toBe('https://example.com:8096/');
+			expect(candidates[2]).toBe('https://example.com:8920/');
 		});
 
 		it('should use the specified port when provided', () => {
 			const candidates = getAddressCandidates('http://example.com:8888');
 			expect(candidates).toHaveLength(2);
-			expect(candidates[0]).toEqual('https://example.com:8888/');
-			expect(candidates[1]).toEqual('http://example.com:8888/');
+			expect(candidates[0]).toBe('https://example.com:8888/');
+			expect(candidates[1]).toBe('http://example.com:8888/');
 		});
 
 		it('should return the entered url non http(s) protocols', () => {
 			const candidates = getAddressCandidates('ftp://example.com');
 			expect(candidates).toHaveLength(1);
-			expect(candidates[0]).toEqual('ftp://example.com/');
+			expect(candidates[0]).toBe('ftp://example.com/');
 		});
 
 		it('should return an empty list for invalid urls', () => {
