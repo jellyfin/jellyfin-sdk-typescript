4cba0ddbd5da03cb589d9f186b037ac1b1d6df5a
Bill Thornton <bill@thornbill.dev>
2021-10-15 15:00:53 -0400

    Remove url-parse dependency

--- jest.config.js
+++ jest.config.js
@@ -1,8 +1,19 @@
 /** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
 module.exports = {
-	preset: 'ts-jest',
-	runner: 'groups',
 	testEnvironment: 'node',
+	runner: 'groups',
+
+	// normalize-url exports an ES module so we need to transform js files
+	// and change the ignore pattern so it is transformed
+	preset: 'ts-jest/presets/js-with-ts',
+	globals: {
+		'ts-jest': {
+			tsconfig: { allowJs: true }
+		}
+	},
+	transformIgnorePatterns: [ '/node_modules/(?!(normalize-url)/)' ],
+
+	// Coverage options
 	collectCoverageFrom: [ 'src/**' ],
 	coveragePathIgnorePatterns: [ '__helpers__', 'generated-client' ]
 };
