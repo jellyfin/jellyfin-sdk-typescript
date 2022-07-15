aabd6ee9860b4f1f5fc5405fbce0bb974f303f5b
Bill Thornton <bill@thornbill.dev>
2022-07-15 11:21:19 -0400

    Add test for ItemSortBy enum

---
+++ src/models/api/__tests__/index.test.ts
@@ -0,0 +1,20 @@
+/**
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
+import { ItemSortBy } from '../index';
+
+/**
+ * Api model tests.
+ * NOTE: This is a really dumb case where because of the way TypeScript enums are generated in JavaScript we need to
+ * interact with the enum in a test (either directly or indirectly) for the file to be considered covered.
+ *
+ * @group unit/models/api
+ */
+describe('ItemSortBy', () => {
+	it('should exist', () => {
+		expect(ItemSortBy).toBeDefined();
+	});
+});
