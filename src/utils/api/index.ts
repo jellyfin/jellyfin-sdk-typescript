210c71ff9e3f396fc7a48c77485c7721b7742a59
Bill Thornton <thornbill@users.noreply.github.com>
2023-03-27 12:18:06 -0400

    Add index for api util functions

---
+++ src/utils/api/index.ts
@@ -0,0 +1,67 @@
+/**
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
+export * from './activity-log-api';
+export * from './api-key-api';
+export * from './artists-api';
+export * from './audio-api';
+export * from './branding-api';
+export * from './channels-api';
+export * from './client-log-api';
+export * from './collection-api';
+export * from './configuration-api';
+export * from './dashboard-api';
+export * from './devices-api';
+export * from './display-preferences-api';
+export * from './dlna-api';
+export * from './dlna-server-api';
+export * from './dynamic-hls-api';
+export * from './environment-api';
+export * from './filter-api';
+export * from './genres-api';
+export * from './hls-segment-api';
+export * from './image-api';
+export * from './image-by-name-api';
+export * from './instant-mix-api';
+export * from './item-lookup-api';
+export * from './item-refresh-api';
+export * from './item-update-api';
+export * from './items-api';
+export * from './library-api';
+export * from './library-structure-api';
+export * from './live-tv-api';
+export * from './localization-api';
+export * from './media-info-api';
+export * from './movies-api';
+export * from './music-genres-api';
+export * from './notifications-api';
+export * from './package-api';
+export * from './persons-api';
+export * from './playlists-api';
+export * from './playstate-api';
+export * from './plugins-api';
+export * from './quick-connect-api';
+export * from './remote-image-api';
+export * from './scheduled-tasks-api';
+export * from './search-api';
+export * from './session-api';
+export * from './startup-api';
+export * from './studios-api';
+export * from './subtitle-api';
+export * from './suggestions-api';
+export * from './sync-play-api';
+export * from './system-api';
+export * from './time-sync-api';
+export * from './tmdb-api';
+export * from './trailers-api';
+export * from './tv-shows-api';
+export * from './universal-audio-api';
+export * from './user-api';
+export * from './user-library-api';
+export * from './user-views-api';
+export * from './video-attachments-api';
+export * from './videos-api';
+export * from './years-api';
